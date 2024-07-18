cba691e6d181a5115c339db54ad3d46465aee6d3
Goh Zhen Rong <60023892+masamune-prog@users.noreply.github.com>
2024-07-18 14:53:49 +0800

    update dependancies

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,29 @@
 """Test cases for the __main__ module."""
 import pytest
 from click.testing import CliRunner
-
-from warc2summary import __main__
+import pandas as pd
+from warc2summary import warc_processor
 
 
 @pytest.fixture
-def runner() -> CliRunner:
-    """Fixture for invoking command-line interfaces."""
-    return CliRunner()
+#add a fixed directory for the warc files
+#check with ashwin which files can be released
+def get_num_websites(directory) -> int:
+    return len(directory.glob("*.warc.gz"))
+
+def check_websites(get_num_websites) -> pd.DataFrame:
+    """Check if the websites are all present."""
+    #get the number of warcc files
+    #check the shape of the dataframe
+    #execute warc processor
+    dataframe = warc_processor.process_warc_files(directory)
+    shape = dataframe.shape[0]
+    assert shape == get_num_websites
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
+
+
+
+def test_main_succeeds(check_websites) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
-    assert True
+    assert check_websites()
